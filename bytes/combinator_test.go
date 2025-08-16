096e47b1636bd37e292aeee2277557d6ad2db4e9
Andrey Zinenko <zinenkoan@yahoo.com>
2025-08-16 10:40:35 +0300

    Rewrite tests for bytes/combinator.go (use table tests), improve test coverage

--- bytes/combinator_test.go
+++ bytes/combinator_test.go
@@ -3,334 +3,581 @@ package bytes
 import (
 	"fmt"
 	"math"
-	"math/rand"
+	"math/rand/v2"
 	"testing"
 	"time"
 
+	ohsnap "github.com/okneniz/oh-snap"
 	"github.com/okneniz/parsec/common"
 	. "github.com/okneniz/parsec/testing"
 )
 
+type (
+	testCase[T any] struct {
+		input  []byte
+		output T
+		err    error
+	}
+
+	test[T any] struct {
+		comb  common.Combinator[byte, int, T]
+		cases []testCase[T]
+	}
+)
+
+func runTests[T comparable](t *testing.T, tests []test[T]) {
+	t.Helper()
+
+	for i, example := range tests {
+		test := example
+		name := fmt.Sprintf("test %d", i)
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			for i, x := range test.cases {
+				testCase := x
+				name := fmt.Sprintf("case %d", i)
+
+				t.Run(name, func(t *testing.T) {
+					result, err := Parse(testCase.input, test.comb)
+
+					if testCase.err != nil {
+						AssertError(t, err)
+						AssertEq(t, err.Error(), testCase.err.Error())
+					} else {
+						Check(t, err)
+					}
+
+					AssertEq(t, result, testCase.output)
+				})
+			}
+		})
+	}
+}
+
+func runTestsSlice[T comparable](t *testing.T, tests []test[[]T]) {
+	t.Helper()
+
+	for i, example := range tests {
+		test := example
+		name := fmt.Sprintf("test %d", i)
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			for i, x := range test.cases {
+				testCase := x
+				name := fmt.Sprintf("case %d", i)
+
+				t.Run(name, func(t *testing.T) {
+					result, err := Parse(testCase.input, test.comb)
+
+					if testCase.err != nil {
+						AssertError(t, err)
+						AssertEq(t, err.Error(), testCase.err.Error())
+					} else {
+						Check(t, err)
+					}
+
+					AssertSlice(t, result, testCase.output)
+				})
+			}
+		})
+	}
+}
+
 func TestSatisfy(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		c := byte('c')
-
-		comb := Satisfy("test", true, func(x byte) bool { return x != c })
-
-		result, err := Parse([]byte("a"), comb)
-		Check(t, err)
-		AssertEq(t, result, 'a')
-
-		result, err = Parse([]byte("b"), comb)
-		Check(t, err)
-		AssertEq(t, result, 'b')
-
-		result, err = Parse([]byte("c"), comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
-	})
-
-	t.Run("case 2", func(t *testing.T) {
-		comb := Satisfy("test", true, func(x byte) bool { return false })
-
-		result, err := Parse([]byte{}, comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
+	runTests(t, []test[byte]{
+		{
+			comb: Satisfy("expected not 'c'", true, func(x byte) bool { return x != 'c' }),
+			cases: []testCase[byte]{
+				{
+					input:  []byte{},
+					output: 0,
+					err:    common.NewParseError(0, "expected not 'c'"),
+				},
+				{
+					input:  []byte("a"),
+					output: 'a',
+				},
+				{
+					input:  []byte("b"),
+					output: 'b',
+				},
+				{
+					input:  []byte("c"),
+					output: 0,
+					err:    common.NewParseError(0, "expected not 'c'"),
+				},
+			},
+		},
+		{
+			comb: Satisfy("error explanation", true, func(x byte) bool { return false }),
+			cases: []testCase[byte]{
+				{
+					input:  []byte{},
+					output: 0,
+					err:    common.NewParseError(0, "error explanation"),
+				},
+				{
+					input:  []byte("abc"),
+					output: 0,
+					err:    common.NewParseError(0, "error explanation"),
+				},
+			},
+		},
 	})
 }
 
 func TestAny(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		source := rand.New(rand.NewSource(time.Now().UnixNano()))
-		comb := Any()
+	comb := Any()
 
-		for i := 0; i < 10000; i++ {
-			b := byte(source.Intn(math.MaxUint8 + 1))
-
-			result, err := Parse([]byte{b}, comb)
-			Check(t, err)
-			AssertEq(t, result, b)
-		}
+	runTests(t, []test[byte]{
+		{
+			comb: comb,
+			cases: []testCase[byte]{
+				{
+					input:  []byte{},
+					output: 0,
+					err:    common.NewParseError(0, common.ErrEndOfFile.Error()),
+				},
+				{
+					input:  []byte("a"),
+					output: 'a',
+				},
+			},
+		},
 	})
 
-	t.Run("case 2", func(t *testing.T) {
-		comb := Any()
+	t.Run("must parse first element from none empty input", func(t *testing.T) {
+		t.Parallel()
 
-		result, err := Parse([]byte{}, comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
+		seed := time.Now().UnixNano()
+		t.Logf("seed: %v", seed)
+		rnd := rand.New(rand.NewPCG(0, uint64(seed)))
+
+		ohsnap.Check(t, 1000, ohsnap.NewBuilder(rnd).Byte(), func(b byte) bool {
+			result, err := Parse([]byte{b}, comb)
+			if err != nil {
+				t.Logf("input: %v", []byte{b})
+				t.Logf("output: %v", result)
+				t.Error(err)
+				return false
+			}
+
+			return result == b
+		})
 	})
 }
 
 func TestTry(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		comb := Try(
-			Satisfy("test", true, func(x byte) bool { return x <= byte('b') }),
-		)
-
-		buf := Buffer([]byte("abcd"))
-		AssertEq(t, buf.Position(), 0)
-
-		result, err := common.Parse[byte, int, byte](buf, comb)
-		Check(t, err)
-		AssertEq(t, result, byte('a'))
-		AssertEq(t, buf.Position(), 1)
-
-		result, err = common.Parse[byte, int, byte](buf, comb)
-		Check(t, err)
-		AssertEq(t, result, byte('b'))
-		AssertEq(t, buf.Position(), 2)
-
-		result, err = common.Parse[byte, int, byte](buf, comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
-		AssertEq(t, buf.Position(), 2)
-
-		result, err = common.Parse[byte, int, byte](buf, comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
-		AssertEq(t, buf.Position(), 2)
+	runTests(t, []test[byte]{
+		{
+			comb: Try(
+				Satisfy(
+					"error explanation",
+					true,
+					func(x byte) bool { return x <= byte('b') },
+				),
+			),
+			cases: []testCase[byte]{
+				{
+					input:  []byte{},
+					output: 0,
+					err:    common.NewParseError(0, "error explanation"),
+				},
+				{
+					input:  []byte("a"),
+					output: 'a',
+				},
+				{
+					input:  []byte("b"),
+					output: 'b',
+				},
+				{
+					input: []byte("c"),
+					err:   common.NewParseError(0, "error explanation"),
+				},
+			},
+		},
 	})
 }
 
 func TestBetween(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		notBrackets := Satisfy("test", true, func(x byte) bool {
-			return !(x == byte(')') || x == byte('('))
-		})
+	notBrackets := Satisfy("test", true, func(x byte) bool {
+		return !(x == byte(')') || x == byte('('))
+	})
 
-		comb := Between(
-			Eq("expected '('", '('),
-			Some(0, "expected not ( or ) symbols", Try(notBrackets)),
-			Eq("expected ')'", ')'),
-		)
+	comb := Between(
+		Eq("expected '('", '('),
+		Some(0, "expected not ( or ) symbols", Try(notBrackets)),
+		Eq("expected ')'", ')'),
+	)
 
-		result, err := Parse([]byte("(abc)"), comb)
-		Check(t, err)
-		AssertSlice(t, result, []byte{'a', 'b', 'c'})
-
-		result, err = Parse([]byte("(abc)def"), comb)
-		Check(t, err)
-		AssertSlice(t, result, []byte{'a', 'b', 'c'})
-
-		result, err = Parse([]byte("(abc))"), comb)
-		Check(t, err)
-		AssertSlice(t, result, []byte{'a', 'b', 'c'})
-
-		result, err = Parse([]byte("(ab)"), comb)
-		Check(t, err)
-		AssertSlice(t, result, []byte{'a', 'b'})
-
-		result, err = Parse([]byte("x(abc)def"), comb)
-		AssertError(t, err)
-		AssertSlice(t, result, nil)
-
-		result, err = Parse([]byte("()"), comb)
-		AssertError(t, err)
-		AssertSlice(t, result, nil)
-
-		result, err = Parse([]byte("(()"), comb)
-		AssertError(t, err)
-		AssertSlice(t, result, nil)
-
-		result, err = Parse([]byte("((1))"), comb)
-		AssertError(t, err)
-		AssertSlice(t, result, nil)
-
-		result, err = Parse([]byte("(abc"), comb)
-		AssertError(t, err)
-		AssertSlice(t, result, nil)
-
-		result, err = Parse([]byte("(abc("), comb)
-		AssertError(t, err)
-		AssertSlice(t, result, nil)
-
-		result, err = Parse([]byte("((abc)"), comb)
-		AssertError(t, err)
-		AssertSlice(t, result, nil)
+	runTestsSlice(t, []test[[]byte]{
+		{
+			comb: comb,
+			cases: []testCase[[]byte]{
+				{
+					input:  []byte{},
+					output: nil,
+					err:    common.NewParseError(0, "expected '('"),
+				},
+				{
+					input:  []byte("(abc)"),
+					output: []byte{'a', 'b', 'c'},
+				},
+				{
+					input: []byte("abc"),
+					err:   common.NewParseError(0, "expected '('"),
+				},
+				{
+					input: []byte("(abc"),
+					err:   common.NewParseError(4, "expected ')'"),
+				},
+				{
+					input: []byte(" (abc) "),
+					err:   common.NewParseError(0, "expected '('"),
+				},
+				{
+					input: []byte("((abc))"),
+					err:   common.NewParseError(1, "expected not ( or ) symbols"),
+				},
+				{
+					input: []byte("()"),
+					err:   common.NewParseError(1, "expected not ( or ) symbols"),
+				},
+			},
+		},
 	})
 }
 
 func TestSkip(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		comb := Skip(
-			Optional(Eq("expected 'a'", 'a'), 0),
-			Eq("expected 'b'", 'b'),
-		)
-
-		result, err := Parse([]byte("abc"), comb)
-		Check(t, err)
-		AssertEq(t, result, byte('b'))
-
-		result, err = Parse([]byte("cba"), comb)
-		Check(t, err)
-		AssertEq(t, result, byte('b'))
+	t.Run("skip not optional", func(t *testing.T) {
+		runTests(t, []test[byte]{
+			{
+				comb: Skip(
+					Eq("expected 'a'", 'a'),
+					Eq("expected 'b'", 'b'),
+				),
+				cases: []testCase[byte]{
+					{
+						input:  []byte{},
+						output: 0,
+						err:    common.NewParseError(0, "expected 'a'"),
+					},
+					{
+						input:  []byte("abc"),
+						output: byte('b'),
+					},
+					{
+						input:  []byte("b"),
+						output: 0,
+						err:    common.NewParseError(0, "expected 'a'"),
+					},
+					{
+						input: []byte("bbb"),
+						err:   common.NewParseError(0, "expected 'a'"),
+					},
+					{
+						input:  []byte("ac"),
+						output: 0,
+						err:    common.NewParseError(1, "expected 'b'"),
+					},
+				},
+			},
+		})
 	})
 
-	t.Run("case 2", func(t *testing.T) {
-		phrase := SequenceOf("expected abc", 'a', 'b', 'c')
-		comb := common.SkipMany(
-			NoneOf("expected not abc", 'a', 'b', 'c'),
-			phrase,
-		)
-
-		result, err := Parse([]byte("abc"), comb)
-		Check(t, err)
-		AssertSlice(t, result, []byte{'a', 'b', 'c'})
-
-		result, err = Parse([]byte("abc123"), comb)
-		Check(t, err)
-		AssertSlice(t, result, []byte{'a', 'b', 'c'})
-
-		result, err = Parse([]byte("123abc"), comb)
-		Check(t, err)
-		AssertSlice(t, result, []byte{'a', 'b', 'c'})
-
-		result, err = Parse([]byte("123abc123"), comb)
-		Check(t, err)
-		AssertSlice(t, result, []byte{'a', 'b', 'c'})
+	t.Run("skip optional", func(t *testing.T) {
+		runTests(t, []test[byte]{
+			{
+				comb: Skip(
+					Optional(Try(Eq("expected 'a'", 'a')), 'x'),
+					Eq("expected 'b'", 'b'),
+				),
+				cases: []testCase[byte]{
+					{
+						input:  []byte{},
+						output: 0,
+						err:    common.NewParseError(0, "expected 'b'"),
+					},
+					{
+						input:  []byte("abc"),
+						output: byte('b'),
+					},
+					{
+						input:  []byte("b"),
+						output: 'b',
+					},
+					{
+						input:  []byte("bbb"),
+						output: 'b',
+					},
+					{
+						input:  []byte("ac"),
+						output: 0,
+						err:    common.NewParseError(1, "expected 'b'"),
+					},
+				},
+			},
+		})
 	})
+}
 
-	t.Run("case 3", func(t *testing.T) {
-		comb := Skip(
-			NotEq("expected not 'a'", 'a'),
-			Eq("expected 'a'", 'a'),
-		)
+func TestSkipMany(t *testing.T) {
+	t.Parallel()
 
-		result, err := Parse([]byte("abc"), comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
+	runTestsSlice(t, []test[[]byte]{
+		{
+			comb: common.SkipMany(
+				NoneOf("expected not a, b or c", 'a', 'b', 'c'),
+				SequenceOf("expected abc", 'a', 'b', 'c'),
+			),
+			cases: []testCase[[]byte]{
+				{
+					input:  []byte{},
+					output: nil,
+					err:    common.NewParseError(0, "expected abc"),
+				},
+				{
+					input:  []byte("abc"),
+					output: []byte("abc"),
+				},
+				{
+					input:  []byte("ab"),
+					output: nil,
+					err:    common.NewParseError(0, "expected abc"),
+				},
+				{
+					input:  []byte("xab"),
+					output: nil,
+					err:    common.NewParseError(1, "expected abc"),
+				},
+				{
+					input:  []byte("123abc"),
+					output: []byte("abc"),
+				},
+				{
+					input:  []byte("bcabc"),
+					output: nil,
+					err:    common.NewParseError(0, "expected abc"),
+				},
+				{
+					input:  []byte("abcabc"),
+					output: []byte("abc"),
+				},
+				{
+					input:  []byte("123abc123"),
+					output: []byte("abc"),
+				},
+				{
+					input:  []byte("123abcabc"),
+					output: []byte("abc"),
+				},
+				{
+					input:  []byte("123"),
+					output: nil,
+					err:    common.NewParseError(3, "expected abc"),
+				},
+			},
+		},
 	})
 }
 
 func TestSkipAfter(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		comb := SkipAfter(
-			Eq("expected 'b'", 'b'),
-			Eq("expected 'a'", 'a'),
-		)
-
-		result, err := Parse([]byte("abc"), comb)
-		Check(t, err)
-		AssertEq(t, result, byte('a'))
-
-		result, err = Parse([]byte("ab"), comb)
-		Check(t, err)
-		AssertEq(t, result, byte('a'))
-
-		result, err = Parse([]byte("a"), comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
-	})
-
-	t.Run("case 2", func(t *testing.T) {
-		comb := SkipAfter(
-			Eq("expected 'b'", 'b'),
-			Satisfy("test", true, common.Nothing[byte]),
-		)
-
-		result, err := Parse([]byte("abc"), comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
-	})
-
-	t.Run("case 3", func(t *testing.T) {
-		comb := SkipAfter(
-			Satisfy("test", true, common.Nothing[byte]),
-			Eq("expected 'a'", 'a'),
-		)
-
-		result, err := Parse([]byte("abc"), comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
+	runTests(t, []test[byte]{
+		{
+			comb: SkipAfter(
+				Eq("expected 'b'", 'b'),
+				Eq("expected 'a'", 'a'),
+			),
+			cases: []testCase[byte]{
+				{
+					input:  []byte{},
+					output: 0,
+					err:    common.NewParseError(0, "expected 'a'"),
+				},
+				{
+					input:  []byte("abc"),
+					output: 'a',
+				},
+				{
+					input:  []byte("ab"),
+					output: 'a',
+				},
+				{
+					input: []byte("a"),
+					err:   common.NewParseError(1, "expected 'b'"),
+				},
+				{
+					input: []byte("ac"),
+					err:   common.NewParseError(1, "expected 'b'"),
+				},
+				{
+					input: []byte("b"),
+					err:   common.NewParseError(0, "expected 'a'"),
+				},
+				{
+					input: []byte("bc"),
+					err:   common.NewParseError(0, "expected 'a'"),
+				},
+				{
+					input: []byte("bb"),
+					err:   common.NewParseError(0, "expected 'a'"),
+				},
+			},
+		},
 	})
 }
 
 func TestPadded(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		comb := Padded(
-			Eq("expected dot", '.'),
-			Range("expected digit between 0 and 9", '0', '9'),
-		)
-
-		result, err := Parse([]byte("1"), comb)
-		Check(t, err)
-		AssertEq(t, result, '1')
-
-		result, err = Parse([]byte(".1"), comb)
-		Check(t, err)
-		AssertEq(t, result, '1')
-
-		result, err = Parse([]byte("...1"), comb)
-		Check(t, err)
-		AssertEq(t, result, '1')
-
-		result, err = Parse([]byte("..1..."), comb)
-		Check(t, err)
-		AssertEq(t, result, '1')
+	runTests(t, []test[byte]{
+		{
+			comb: Padded(
+				Eq("expected dot", '.'),
+				Range("expected digit", '0', '9'),
+			),
+			cases: []testCase[byte]{
+				{
+					input:  []byte{},
+					output: 0,
+					err:    common.NewParseError(0, "expected digit"),
+				},
+				{
+					input:  []byte("1"),
+					output: '1',
+				},
+				{
+					input:  []byte(".1"),
+					output: '1',
+				},
+				{
+					input:  []byte(".1."),
+					output: '1',
+				},
+				{
+					input:  []byte("...1.."),
+					output: '1',
+				},
+				{
+					input:  []byte("x...1.."),
+					output: 0,
+					err:    common.NewParseError(0, "expected digit"),
+				},
+				{
+					input:  []byte("...1..x"),
+					output: '1',
+				},
+			},
+		},
 	})
 }
 
 func TestEOF(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		result, err := Parse([]byte("abcd"), EOF())
-		Check(t, err)
-		AssertEq(t, result, false)
-	})
-
-	t.Run("case 2", func(t *testing.T) {
-		result, err := Parse([]byte(""), EOF())
-		Check(t, err)
-		AssertEq(t, result, true)
+	runTests(t, []test[bool]{
+		{
+			comb: EOF(),
+			cases: []testCase[bool]{
+				{
+					input:  []byte{},
+					output: true,
+				},
+				{
+					input:  []byte("1"),
+					output: false,
+				},
+				{
+					input:  []byte("123"),
+					output: false,
+				},
+			},
+		},
 	})
 }
 
 func TestCast(t *testing.T) {
 	t.Parallel()
 
-	t.Run("case 1", func(t *testing.T) {
-		comb := Cast(
-			Satisfy("test", true, common.Anything[byte]),
-			func(x byte) (int, error) { return int(x), nil },
-		)
-
-		result, err := Parse([]byte("a"), comb)
-		Check(t, err)
-		AssertEq(t, result, 97)
-
-		result, err = Parse([]byte("b"), comb)
-		Check(t, err)
-		AssertEq(t, result, 98)
-
-		result, err = Parse([]byte(""), comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
-	})
-
-	t.Run("case 2", func(t *testing.T) {
-		comb := Cast(
-			Any(),
-			func(x byte) (int, error) { return -1, fmt.Errorf("test error") },
-		)
-
-		result, err := Parse([]byte("abc"), comb)
-		AssertError(t, err)
-		AssertEq(t, result, 0)
+	runTests(t, []test[int]{
+		{
+			comb: Cast(
+				Satisfy("test", true, common.Anything[byte]),
+				func(x byte) (int, error) { return int(x), nil },
+			),
+			cases: []testCase[int]{
+				{
+					input:  []byte{},
+					output: 0,
+					err:    common.NewParseError(0, "test"),
+				},
+				{
+					input:  []byte{0},
+					output: 0,
+				},
+				{
+					input:  []byte{1},
+					output: 1,
+				},
+				{
+					input:  []byte{math.MaxUint8},
+					output: 255,
+				},
+				{
+					input:  []byte{math.MaxInt8},
+					output: 127,
+				},
+			},
+		},
+		{
+			comb: Cast(
+				Any(),
+				func(x byte) (int, error) { return -1, fmt.Errorf("test error") },
+			),
+			cases: []testCase[int]{
+				{
+					input:  []byte{},
+					output: 0,
+					err:    common.NewParseError(0, common.ErrEndOfFile.Error()),
+				},
+				{
+					input:  []byte{0},
+					output: 0,
+					err:    common.NewParseError(0, "test error"),
+				},
+				{
+					input:  []byte{1},
+					output: 0,
+					err:    common.NewParseError(0, "test error"),
+				},
+				{
+					input:  []byte{math.MaxUint8},
+					output: 0,
+					err:    common.NewParseError(0, "test error"),
+				},
+				{
+					input:  []byte{math.MaxInt8},
+					output: 0,
+					err:    common.NewParseError(0, "test error"),
+				},
+			},
+		},
 	})
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/okneniz/parsec
 go 1.23.2
 
 require (
-	github.com/okneniz/oh-snap v0.0.0-20250811170615-1724911f5d7c
+	github.com/okneniz/oh-snap v0.0.0-20250815201657-df20b028e967
 	github.com/vmihailenco/msgpack/v5 v5.3.5
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
 )
