7a3e70f58ef7ae04ce61ad4334ab77e54e78d536
Andrey Zinenko <zinenkoan@yahoo.com>
2025-08-24 13:53:19 +0300

    Move JSEq to example package from tests

--- examples/strings/json/json_test.go
+++ examples/strings/json/json_test.go
@@ -1,7 +1,6 @@
 package json
 
 import (
-	"fmt"
 	"math/rand/v2"
 	"testing"
 	"time"
@@ -40,7 +39,7 @@ func TestJSONParsing(t *testing.T) {
 				return false
 			}
 
-			if !isJSEq(data, result) {
+			if !JSEq(data, result) {
 				t.Log("data", data)
 				t.Log("json", str)
 				t.Log("parsed data", result)
@@ -50,78 +49,4 @@ func TestJSONParsing(t *testing.T) {
 			return true
 		})
 	})
-}
-
-func isJSEq(x, y JSON) bool {
-	if fmt.Sprintf("%T", x) != fmt.Sprintf("%T", y) {
-		return false
-	}
-
-	switch xv := x.(type) {
-	case JSBool:
-		yv, ok := y.(JSBool)
-		if !ok {
-			return false
-		}
-
-		return xv.value == yv.value
-	case JSString:
-		yv, ok := y.(JSString)
-		if !ok {
-			return false
-		}
-
-		return xv.value == yv.value
-	case JSNumber:
-		yv, ok := y.(JSNumber)
-		if !ok {
-			return false
-		}
-
-		return xv.value == yv.value
-	case JSNull:
-		_, ok := y.(JSNull)
-		return ok
-	case JSArray:
-		yv, ok := y.(JSArray)
-		if !ok {
-			return false
-		}
-
-		if len(xv.values) != len(yv.values) {
-			return false
-		}
-
-		for i := 0; i < len(xv.values); i++ {
-			if !isJSEq(xv.values[i], yv.values[i]) {
-				return false
-			}
-		}
-
-		return true
-	case JSObject:
-		yv, ok := y.(JSObject)
-		if !ok {
-			return false
-		}
-
-		if len(xv.values) != len(yv.values) {
-			return false
-		}
-
-		for key, xvalue := range xv.values {
-			yvalue, exists := yv.values[key]
-			if !exists {
-				return false
-			}
-
-			if !isJSEq(xvalue, yvalue) {
-				return false
-			}
-		}
-
-		return true
-	}
-
-	return false
 }
--- examples/strings/json/types.go
+++ examples/strings/json/types.go
@@ -106,3 +106,77 @@ type JSPair struct {
 	key   JSString
 	value JSON
 }
+
+func JSEq(x, y JSON) bool {
+	if fmt.Sprintf("%T", x) != fmt.Sprintf("%T", y) {
+		return false
+	}
+
+	switch xv := x.(type) {
+	case JSBool:
+		yv, ok := y.(JSBool)
+		if !ok {
+			return false
+		}
+
+		return xv.value == yv.value
+	case JSString:
+		yv, ok := y.(JSString)
+		if !ok {
+			return false
+		}
+
+		return xv.value == yv.value
+	case JSNumber:
+		yv, ok := y.(JSNumber)
+		if !ok {
+			return false
+		}
+
+		return xv.value == yv.value
+	case JSNull:
+		_, ok := y.(JSNull)
+		return ok
+	case JSArray:
+		yv, ok := y.(JSArray)
+		if !ok {
+			return false
+		}
+
+		if len(xv.values) != len(yv.values) {
+			return false
+		}
+
+		for i := 0; i < len(xv.values); i++ {
+			if !JSEq(xv.values[i], yv.values[i]) {
+				return false
+			}
+		}
+
+		return true
+	case JSObject:
+		yv, ok := y.(JSObject)
+		if !ok {
+			return false
+		}
+
+		if len(xv.values) != len(yv.values) {
+			return false
+		}
+
+		for key, xvalue := range xv.values {
+			yvalue, exists := yv.values[key]
+			if !exists {
+				return false
+			}
+
+			if !JSEq(xvalue, yvalue) {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
